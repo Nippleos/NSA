a026796d1d192f5a77d956dc4f76216494d4ba99
2bitch <mail.goran96@gmail.com>
2015-03-13 15:08:15 +0100

    Login
    ---

--- login.php
+++ login.php
@@ -1,14 +1,30 @@
 <?php
-
+session_start();
+if(isset($_POST['submit'])){
+	global $conn;
+	$conn=new mysqli('localhost','admgorant','nahod5afekt','anaslex');
+	if ($conn->connect_error) {
+		$error_message="Napaka pri povezavi z bazo: ".$conn->connect_error;
+		echo '<script type="text/javascript">alert("'.$error_message.'");</script>';
+		header('Location: login.php');
+		exit();
+	}else{
+		header('Location: first.php');
+	}
+}
 
 
 ?><!DOCTYPE html>
 <html>
 <head>
-
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
 
 <body>
-
+<form method="post" action="login.php">
+	Uporabniško ime: <input type="text" name="username"><br>
+	Geslo: <input type="password" name="password"><br>
+	<input type="submit" name="submit">
+</form>
 </body>
 </html>
