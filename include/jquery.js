99ec293e0237e6128c4aa428fc2877052ec31030
2bitch <mail.goran96@gmail.com>
2015-04-22 11:47:19 +0200

    sacxy
    SCXA

--- include/jquery.js
+++ include/jquery.js
@@ -23,9 +23,10 @@ $('#glyphicon-registration-mark').on('click',function(){
 			if("empty" in data){
 				$('#new_users_table').empty();
 				$('#new_users_table').append('<th>'+data['empty']+'</th>');
+				return;
 			}
 			$.each(data,function(key,value){		
-				$('#new_users_table').append('<tr id="new_users_tr'+value["UserID"]+'"><td><input type="checkbox" name="hmm" value="'+value["UserID"]+'"></td><td>'+value["UserID"]+'</td><td>'+value["Username"]+'</td><td>'+value["Name"]+'</td><td>'+value["Surname"]+'</td><td>'+value["Emso"]+'</td><td>'+value["StatusID"]+'</td><td><a id="glyphicon-remove'+value["UserID"]+'" href="#"><span class="glyphicon glyphicon-remove"></span></a></td></tr>');
+				$('#new_users_table').append('<tr id="new_users_tr'+value["UserID"]+'"><td><input type="checkbox" name="hmm" value="'+value["UserID"]+'"></td><td>'+value["UserID"]+'</td><td>'+value["Username"]+'</td><td>'+value["Name"]+'</td><td>'+value["Surname"]+'</td><td>'+value["Emso"]+'</td><td>'+value["StatusID"]+'</td><td><a id="glyphicon-remove'+value["UserID"]+'" href="#"><span class="glyphicon glyphicon-remove"></span></a> <a id="glyphicon-plus'+value["UserID"]+'" href="#"><span class="glyphicon glyphicon-plus"></span></a></td></tr>');
 				$('#glyphicon-remove'+value["UserID"]).on('click',function(){
 					var id=$(this).attr('id').replace('glyphicon-remove','');
 					var xmlhttp=new XMLHttpRequest();
@@ -42,8 +43,35 @@ $('#glyphicon-registration-mark').on('click',function(){
 					xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 					xmlhttp.send(parameters);
 				});
+				$('#glyphicon-plus'+value["UserID"]).on('click',function(){
+					var id=$(this).attr('id').replace('glyphicon-plus','');
+					var xmlhttp=new XMLHttpRequest();
+					var parameters="name=checkuser&id="+id;
+					xmlhttp.onreadystatechange=function(){
+						if(xmlhttp.readyState==4 && xmlhttp.status==200){
+							var data=JSON.parse(xmlhttp.responseText);
+							if(data===1){
+								$('#new_users_tr'+id).remove();
+							}
+						}
+					}
+					xmlhttp.open("POST","ajax/ajax_new_users.php",true);
+					xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+					xmlhttp.send(parameters);
+				});
 			});
-			$('#new_users_table').append('<tr id="new_users_lastrow"><td><a id="glyphicon-trash" href="#"><span class="glyphicon glyphicon-trash"></span></a></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
+			$('#new_users_table').append('<tr id="new_users_lastrow"><td colspan="8"><img src="images/arrow_ltr.png" />&nbsp<input type="checkbox"> Mark all</input> <i style="margin-left:30px; margin-right:10px;">With marked: </i><a id="glyphicon-trash" href="#"><span class="glyphicon glyphicon-trash"></span></a> Delete requests <a><span class="glyphicon glyphicon-ok"></span></a> Confirm requests</td></tr>');
+			$('#new_users_lastrow input[type=checkbox]').on('click',function(){
+				if($('#new_users_lastrow input[type=checkbox]:checked').length){
+					$('#new_users_table td input[name=hmm]').each(function(){
+						$(this).prop('checked', true);
+					});
+				}else{
+					$('#new_users_table td input[name=hmm]').each(function(){
+						$(this).prop('checked', false);
+					});
+				}
+			});
 			$('#new_users_lastrow #glyphicon-trash').on('click',function(){
 				var vsi=[];
 				$('#new_users_table input[type=checkbox]:checked').each(function(){
